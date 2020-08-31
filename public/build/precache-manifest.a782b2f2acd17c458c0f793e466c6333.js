50cb39d959edf07b383832af5f40f9ad8ecc1b4c
Kabeer Jaffri <52799968+kabeer11000@users.noreply.github.com>
2020-08-31 09:45:33 +0500

    Add files via upload

---
+++ public/build/precache-manifest.a782b2f2acd17c458c0f793e466c6333.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+    {
+        "revision": "1a1f9dd07abc84dc64e2",
+        "url": "/static/css/main.9b985559.chunk.css"
+    },
+    {
+        "revision": "1a1f9dd07abc84dc64e2",
+        "url": "/static/js/main.382f8d17.chunk.js"
+    },
+    {
+        "revision": "7b3278a3b1d31a3b37bf",
+        "url": "/static/js/runtime~main.05f8dc37.js"
+    },
+    {
+        "revision": "69ddcb37f4e502505a93",
+        "url": "/static/js/2.d66c296d.chunk.js"
+    },
+    {
+        "revision": "a76de4b8be384a2d46eb",
+        "url": "/static/js/3.122bf8a2.chunk.js"
+    },
+    {
+        "revision": "75611a521817a47d6564",
+        "url": "/static/css/4.3c2c4944.chunk.css"
+    },
+    {
+        "revision": "75611a521817a47d6564",
+        "url": "/static/js/4.5dfff005.chunk.js"
+    },
+    {
+        "revision": "812b208f81c99fcf99fd",
+        "url": "/static/js/5.1b860432.chunk.js"
+    },
+    {
+        "revision": "6cf477ee54fc4bde3191",
+        "url": "/static/css/6.e08351d1.chunk.css"
+    },
+    {
+        "revision": "6cf477ee54fc4bde3191",
+        "url": "/static/js/6.bf71ecb7.chunk.js"
+    },
+    {
+        "revision": "2372cd27b7b5ca71f5ba",
+        "url": "/static/css/7.c532123e.chunk.css"
+    },
+    {
+        "revision": "2372cd27b7b5ca71f5ba",
+        "url": "/static/js/7.dbd9138a.chunk.js"
+    },
+    {
+        "revision": "37addf39b8fc879ff99f",
+        "url": "/static/css/8.4bd2de7f.chunk.css"
+    },
+    {
+        "revision": "37addf39b8fc879ff99f",
+        "url": "/static/js/8.d909e8b5.chunk.js"
+    },
+    {
+        "revision": "4791cdf4ea0212bd43be",
+        "url": "/static/css/9.29611205.chunk.css"
+    },
+    {
+        "revision": "4791cdf4ea0212bd43be",
+        "url": "/static/js/9.cbda34e5.chunk.js"
+    },
+    {
+        "revision": "5a7c70a540409a1d19bd",
+        "url": "/static/css/10.a0b031fc.chunk.css"
+    },
+    {
+        "revision": "5a7c70a540409a1d19bd",
+        "url": "/static/js/10.5fad8ffe.chunk.js"
+    },
+    {
+        "revision": "4c54b609d4f52488afc7",
+        "url": "/static/js/11.b4d3a786.chunk.js"
+    },
+    {
+        "revision": "9acac5c95fae95757cfd",
+        "url": "/static/js/12.1d084478.chunk.js"
+    },
+    {
+        "revision": "135958d69e615bb8fb60cdd896f314f3",
+        "url": "/index.html"
+    }
+];
