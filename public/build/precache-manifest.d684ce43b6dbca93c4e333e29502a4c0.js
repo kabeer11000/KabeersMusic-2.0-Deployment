4ec814f840eff8a0a073564cc61037c97e9aa34f
kabeer11000 <52799968+kabeer11000@users.noreply.github.com>
2020-09-15 19:21:39 +0500

    Third Commit - Fuck Heroku

---
+++ public/build/precache-manifest.d684ce43b6dbca93c4e333e29502a4c0.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b790db6aabf8b0ac8f5",
+    "url": "./static/js/0.e4e88809.chunk.js"
+  },
+  {
+    "revision": "3fa8a34635074e9d30a7",
+    "url": "./static/js/1.c59e832d.chunk.js"
+  },
+  {
+    "revision": "0f4cbb78bce0d4351607",
+    "url": "./static/css/main.eb47b841.chunk.css"
+  },
+  {
+    "revision": "0f4cbb78bce0d4351607",
+    "url": "./static/js/main.074a25ef.chunk.js"
+  },
+  {
+    "revision": "268b90c61319fe8fa079",
+    "url": "./static/js/runtime~main.e47e8e96.js"
+  },
+  {
+    "revision": "41cb8d804d473c71ad92",
+    "url": "./static/css/4.1331623a.chunk.css"
+  },
+  {
+    "revision": "41cb8d804d473c71ad92",
+    "url": "./static/js/4.281ad360.chunk.js"
+  },
+  {
+    "revision": "cd3dbace8336fdc31ded",
+    "url": "./static/js/5.232ee46e.chunk.js"
+  },
+  {
+    "revision": "2019de3d3a63e1562619",
+    "url": "./static/css/6.29611205.chunk.css"
+  },
+  {
+    "revision": "2019de3d3a63e1562619",
+    "url": "./static/js/6.521f6355.chunk.js"
+  },
+  {
+    "revision": "035c38ebe0d60a2fee4a",
+    "url": "./static/js/7.ff0d0025.chunk.js"
+  },
+  {
+    "revision": "0904d13c1f364fd65186",
+    "url": "./static/css/8.1b21d234.chunk.css"
+  },
+  {
+    "revision": "0904d13c1f364fd65186",
+    "url": "./static/js/8.dd68e813.chunk.js"
+  },
+  {
+    "revision": "48967b566cd75e1a4fa6",
+    "url": "./static/css/9.08966487.chunk.css"
+  },
+  {
+    "revision": "48967b566cd75e1a4fa6",
+    "url": "./static/js/9.23e3ad77.chunk.js"
+  },
+  {
+    "revision": "17cd94afd71b922ca8d6",
+    "url": "./static/css/10.d12f48ee.chunk.css"
+  },
+  {
+    "revision": "17cd94afd71b922ca8d6",
+    "url": "./static/js/10.9630ad7b.chunk.js"
+  },
+  {
+    "revision": "e950ca9dd362b368689e",
+    "url": "./static/css/11.49ec9360.chunk.css"
+  },
+  {
+    "revision": "e950ca9dd362b368689e",
+    "url": "./static/js/11.afb11465.chunk.js"
+  },
+  {
+    "revision": "cde6a00b96e62aff8537",
+    "url": "./static/css/12.e08351d1.chunk.css"
+  },
+  {
+    "revision": "cde6a00b96e62aff8537",
+    "url": "./static/js/12.22d7c8ed.chunk.js"
+  },
+  {
+    "revision": "2f0e7302e0a13fe6b1f8",
+    "url": "./static/css/13.a9166dc2.chunk.css"
+  },
+  {
+    "revision": "2f0e7302e0a13fe6b1f8",
+    "url": "./static/js/13.e98e3329.chunk.js"
+  },
+  {
+    "revision": "b09721e758f788447c3b",
+    "url": "./static/css/14.948232b0.chunk.css"
+  },
+  {
+    "revision": "b09721e758f788447c3b",
+    "url": "./static/js/14.55c3e65a.chunk.js"
+  },
+  {
+    "revision": "4aab2acd874b93b3d7af",
+    "url": "./static/css/15.0de8d54d.chunk.css"
+  },
+  {
+    "revision": "4aab2acd874b93b3d7af",
+    "url": "./static/js/15.673fe463.chunk.js"
+  },
+  {
+    "revision": "2ee2cc78a169d35988fb",
+    "url": "./static/css/16.4e9f5285.chunk.css"
+  },
+  {
+    "revision": "2ee2cc78a169d35988fb",
+    "url": "./static/js/16.7215c43b.chunk.js"
+  },
+  {
+    "revision": "8a18371f89cce6ea02f7",
+    "url": "./static/js/17.91543316.chunk.js"
+  },
+  {
+    "revision": "ee318cd9f1a6df37f902",
+    "url": "./static/css/18.806e5f88.chunk.css"
+  },
+  {
+    "revision": "ee318cd9f1a6df37f902",
+    "url": "./static/js/18.6e301132.chunk.js"
+  },
+  {
+    "revision": "dc7c6290eda54f930996",
+    "url": "./static/js/19.e434326e.chunk.js"
+  },
+  {
+    "revision": "093f37d35e91281afe1d",
+    "url": "./static/js/20.2522cc5d.chunk.js"
+  },
+  {
+    "revision": "3b335d1aa9ac3f9c2915d627f19d5bc5",
+    "url": "./index.html"
+  }
+];
--- public/build/service-worker.js
+++ public/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8cdeb643b5ca14ee6bbb43e7a87dbb36.js"
+  "./precache-manifest.d684ce43b6dbca93c4e333e29502a4c0.js"
 );
 
 workbox.clientsClaim();
